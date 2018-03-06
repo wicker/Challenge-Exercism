e2eba62bcd583145dbb3a8065b5ed51e975d5f58
Jenner Hanni <jenner@wickerbox.net>
2018-03-06 10:16:13 -0800

    submit: Exercise 2

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,12 +1,17 @@
 var TwoFer = function () {};
 
 TwoFer.prototype.twoFer = function (who) {
-  // your code goes here
-  // You will have to use the parameter who
-  // in some way. In this example, it is just
-  // returned, but your solution will have to
-  // use a conditional.
-  return who;
-};
+
+  // Uses a template literal, which is a string
+  // allowing embedded expressions. Used to be
+  // called template strings.
+  //
+  // $ is a valid identifier in JS
+  // which returns equivalent to
+  //   (who === undefined || who === null || who === '')
+
+  return `One for ${who || "you"}, one for me.`;
+
+}
 
 module.exports = TwoFer;
